44b331cd48cd9f55779ad12812bae9c8ec4bbf9b
Adam Lugowski <alugowski@gmail.com>
2023-08-24 12:29:05 -0700

    Clarify tabulate post-process code

--- matrepr/string_formatter.py
+++ matrepr/string_formatter.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by the BSD 2-clause license found in the LICENSE.txt file.
 # SPDX-License-Identifier: BSD-2-Clause
 
-from typing import Any, Dict, Tuple
+from typing import Any, Dict, Tuple, Optional
 from tabulate import tabulate, TableFormat, Line, DataRow
 
 from . import params, _get_adapter
@@ -10,13 +10,13 @@ from .list_converter import ListConverter
 
 matrix_table_format = TableFormat(
     lineabove=Line("┌", " ", " ", "┐"),
-    linebelowheader=None,
+    linebelowheader=Line("┌", " ", " ", "┐"),
     linebetweenrows=None,
     linebelow=Line("└", " ", " ", "┘"),
-    headerrow=DataRow(" ", " ", ""),
+    headerrow=DataRow(" ", " ", " "),
     datarow=DataRow("│", " ", "│"),
     padding=1,
-    with_header_hide=None,
+    with_header_hide=["lineabove"],
 )
 """
 Renders table that looks like a LaTeX bmatrix.
@@ -29,38 +29,40 @@ matrix_format_patched_indices = TableFormat(
     linebelowheader=Line("", " ", "┌", "┐"),
     linebetweenrows=None,
     linebelow=Line("", " ", "└", "┘"),
-    headerrow=DataRow("", "│", ""),
+    headerrow=DataRow("", " ", ""),
     datarow=DataRow("", "│", "│"),
     padding=1,
     with_header_hide=None,
 )
 """
-A corrupted version of :ref:`matrix_table_format` that, when patched using :func:`_patch_index_tabulate_output`
+A corrupted version of :ref:`matrix_table_format` that, when processed using :func:`_tabulate_sep_to_border`,
 yields the same beautiful bmatrix but with indices nicely outside the brackets.
 """
 
 
-def _patch_index_tabulate_output(s: str) -> str:
+def _tabulate_sep_to_border(s: str, tf: TableFormat) -> str:
     lines = s.split("\n")
 
-    # header only has separators
-    lines[0] = lines[0].replace("│", " ")
-
-    # below-header row, data rows, bottom row
+    # Must correct below-header row, data rows, bottom row.
     for row in range(1, len(lines)):
         line = lines[row]
+        save_inds = []
 
-        for needle in ["┌", "│", "└", None]:
-            if line.find(needle) >= 0:
-                break
-
-        if needle is None:
-            continue
-
-        save_inds = [line.find(needle)]
-        if line.endswith(needle):
+        if row == 1:
+            # below-header row
+            needle = tf.linebelowheader.sep
+        elif row < len(lines) - 1:
+            # data row uses | separator which appears at the very end too
+            needle = tf.datarow.sep
             save_inds.append(-1)
+        else:
+            # bottom row
+            needle = tf.linebelow.sep
 
+        # The leftmost separator is actually the matrix border, so save that one
+        save_inds.append(line.find(needle))
+
+        # convert all but the one or two saved copies of the separator to spaces
         line_chars = list(line.replace(needle, " "))
         for i in save_inds:
             line_chars[i] = needle
@@ -74,18 +76,18 @@ def _labels_to_str(labels):
         yield "" if label is None else str(label)
 
 
-def _to_tabulate_args(mat: Any, **kwargs) -> Tuple[Dict, bool]:
+def _to_tabulate_args(mat: Any, **kwargs) -> Tuple[Dict, Optional[TableFormat]]:
     """
     Creates arguments for :func:`~tabulate.tabulate` to format the matrix.
 
     Note that tabulate is not expressive enough to render a matrix with nice row indices. Tabulate does not distinguish
     between the index column and data columns. Therefore, index output renders a corrupted output that is then patched
-    using :func:`_patch_index_tabulate_output`.
+    using :func:`_tabulate_sep_to_border`.
 
     :param mat: A supported matrix.
     :param kwargs: Any argument in :class:`MatReprParams`, or any argument supported by :func:`~tabulate.tabulate`.
-    :return: the argument dictionary and a flag. If the flag is True then :func:`_patch_index_tabulate_output` must be
-             called on the output.
+    :return: the argument dictionary and an object. If the object is not None then :func:`_tabulate_sep_to_border`
+             must be called with the tabulate output and this object.
     """
     options = params.get(**kwargs)
     adapter = _get_adapter(mat)
@@ -93,13 +95,13 @@ def _to_tabulate_args(mat: Any, **kwargs) -> Tuple[Dict, bool]:
     # determine the table format
     if "tablefmt" in kwargs:
         matrix_format = kwargs["tablefmt"]
-        patch_required = False
+        patch_required = None
     elif options.indices:
         matrix_format = matrix_format_patched_indices
-        patch_required = True
+        patch_required = matrix_format_patched_indices
     else:
         matrix_format = matrix_table_format
-        patch_required = False
+        patch_required = None
 
     # collect the data
     conv = ListConverter(**options.to_kwargs())
@@ -145,9 +147,9 @@ def to_tabulate(mat: Any, **kwargs) -> str:
     :param kwargs: Any argument in :class:`MatReprParams`, or any argument supported by :func:`~tabulate.tabulate`.
     :return: output of :func:`~tabulate.tabulate`
     """
-    args, patch_required = _to_tabulate_args(mat, **kwargs)
+    args, patch_tf = _to_tabulate_args(mat, **kwargs)
     ret = tabulate(**args)
 
-    if patch_required:
-        ret = _patch_index_tabulate_output(ret)
+    if patch_tf:
+        ret = _tabulate_sep_to_border(ret, patch_tf)
     return ret
