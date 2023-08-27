60a1310a70c500a00187b744bee74d9910c4c8b3
Adam Lugowski <alugowski@gmail.com>
2023-08-27 00:29:15 -0700

    Add missing copyright

--- matrepr/adapters/graphblas_impl.py
+++ matrepr/adapters/graphblas_impl.py
@@ -1,3 +1,7 @@
+# Copyright (C) 2023 Adam Lugowski.
+# Use of this source code is governed by the BSD 2-clause license found in the LICENSE.txt file.
+# SPDX-License-Identifier: BSD-2-Clause
+
 from typing import Any, Iterable, Tuple
 
 import graphblas as gb
